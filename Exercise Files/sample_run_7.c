b7f4b0de666437d819349deb74756dee573f9360
Channveer Mulge <130136525+c-mulge@users.noreply.github.com>
2023-04-07 16:38:28 +0530

    Add files via upload

---
+++ Exercise Files/sample_run_7.c
@@ -0,0 +1,13 @@
+//for loop 
+//practice
+#include<stdio.h>
+int main()
+{
+    int a=1;
+    //here a is the iterartor or counter
+    for(a; a<=10; a= a+1)
+    {
+        printf("%d\n",a);
+    }
+    return 0;
+}
