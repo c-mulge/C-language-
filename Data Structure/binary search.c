39cdca0d175e36c8009c85500fe5c236966815a5
Channveer Mulge <130136525+c-mulge@users.noreply.github.com>
2024-01-14 19:41:41 +0530

    Update binary search.c

--- Data Structure/binary search.c
+++ Data Structure/binary search.c
@@ -3,7 +3,7 @@
 #include<stdlib.h>
 #define maxsz 50
 
-int binary(int a[],int lb,int ub,int key)     //for ascending order
+int binary(int a[],int lb,int ub,int key)
 {
     int mid;
     while(lb<=ub)
@@ -19,65 +19,74 @@ int binary(int a[],int lb,int ub,int key)     //for ascending order
     return -1;
 }
 
-int binary(int a[],int ub,int lb,int key)         //for descending order
+void accept(int a[],int n)
 {
-    int mid;
-    while(ub<=lb)
+    int i,j;
+    int hold;
+    for(i=0;i<n;i++)
     {
-        mid=(lb+ub)/2;
-        if(a[mid]==key)
-            return mid;
-        else if(a[mid]>key)
-                ub=mid+1;
-            else
-                lb=mid-1;
+        scanf("%d",&hold);
+        for(j=i-1;j>=0&&hold<a[j];j--)
+            a[j+1]=a[j];
+        a[j+1]=hold;
     }
-    return -1;
 }
 
-int main() 
+void display(int a[],int n)
 {
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+
+int main()
+{
+    int choice;
     int a[maxsz];
-    int n;
-    printf("Size of array: ");
-    scanf("%d",&n);
-    printf("Enter %d numbers: ",n);
-    for(int i=0; i<n; i++)
+    int n,key;
+    
+    while(1)
     {
-        scanf("%d",&a[i]);
-    }
-    int i,j;
-    int hold;              
-    for (i = 1; i < n; i++) 
-    {
-        hold = a[i];
-        for(j = i - 1; j >= 0 && a[j] > hold; j--)        //change sign to a[j] < hold
+        printf("\nOperations are: ");
+        printf("\n1.Insert");
+        printf("\n2.Find key");
+        printf("\n3.Display");
+        printf("\n4.Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d",&choice);
+        switch(choice)
         {
-            a[j + 1] = a[j];
-        }
-            a[j + 1] = hold;
-    }
-
-    printf("Sorted: \n");
-            for(int i=0;i<n;i++)
+        case 1:
+            printf("Size of array: ");
+            scanf("%d",&n);
+            printf("Enter %d numbers: ",n);
+            accept(a,n);
+            break;
+        case 2:
+            printf("Enter key to find: ");
+            scanf("%d",&key);
+            int result=binary(a,0,n-1,key);
+            if(result==-1)
             {
-                printf("%d ",a[i]);
+                printf("The %d key did not found in the array.\n",key);
             }
-
-    int key;
-
-    printf("\nEnter the element to search: ");
-    scanf("%d", &key);
-
-    int result = binary(a, 0, n - 1, key);
-
-    if (result != -1) 
-    {
-        printf("Element %d found at index %d.\n", key, result);
-    } 
-    else 
-    {
-        printf("Element %d not found in the array.\n", key);
+            else
+            {
+                printf("The %d key is at %d result \n",key,result);
+            }
+            break;
+        case 3:
+            printf("\nElements: ");
+            display(a,n);
+            break;
+        case 4:
+            exit(1);
+            break;
+        default:
+            printf("Invalid choice");
+        }
     }
     return 0;
 }
